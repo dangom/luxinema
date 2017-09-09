417bcc369fe65c75099dd3c67ec50d88b4f4581d
Daniel Gomez <d.gomez@posteo.org>
2017-09-09 16:43:42 +0200

    Update test requirements to use hypothesis.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     ],
 
     install_requires=REQUIRES,
-    tests_require=['coverage', 'pytest'],
+    tests_require=['coverage', 'pytest', 'hypothesis'],
 
     packages=find_packages(),
 )
